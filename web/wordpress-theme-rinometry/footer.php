3bfb88e23c1163bb383c07d70880aee22d476bad
Rafael2712 <canelonrafael@gmail.com>
2026-01-22 19:52:23 +0100

    Replace header/footer logos with new assets

--- web/wordpress-theme-rinometry/footer.php
+++ web/wordpress-theme-rinometry/footer.php
@@ -4,7 +4,7 @@
 <footer class="site-footer" role="contentinfo">
   <div class="container footer-simple">
     <div class="footer-logo">
-      <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/img/logo-footer.png'); ?>" alt="<?php esc_attr_e('Rhinometric', 'rinometry'); ?>" />
+      <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/img/footer.png'); ?>" alt="<?php esc_attr_e('Rhinometric', 'rinometry'); ?>" />
     </div>
     <p><?php esc_html_e('© Rhinometric. All rights reserved.', 'rinometry'); ?></p>
     <p><?php esc_html_e('Contact: ', 'rinometry'); ?><a href="mailto:rafael.canelon@rhinometric.com">rafael.canelon@rhinometric.com</a></p>
